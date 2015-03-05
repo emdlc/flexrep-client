25cebbd95808a6acfa80a0c6d485b9b1627ea0f2
Ed Delacruz <ed.delacruz@marklogic.com>
2015-03-05 17:13:26 -0500

    HTTP Check to see if Tomcat is responding to apply.xqy servlet

--- flexrep-client/src/test/java/com/marklogic/flexrep/client/FileSystemApplyServletTest.java
+++ flexrep-client/src/test/java/com/marklogic/flexrep/client/FileSystemApplyServletTest.java
@@ -6,7 +6,12 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
+import java.util.logging.Logger;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -22,35 +27,40 @@ import com.marklogic.xcc.Session;
 import com.marklogic.xcc.exceptions.RequestException;
 import com.marklogic.xcc.exceptions.XccConfigException;
 
-public class ApplyServletTest {
+public class FileSystemApplyServletTest {
 
-	private static final String NON_CHUNKING_FILENAME = "canyon-single.jpg";
+	private static final Logger logger = Logger
+			.getLogger(FileSystemApplyServletTest.class.getName());
+
+	private static final String NON_CHUNKING_FILENAME = "/canyon-single.jpg";
 
 	@Test
 	public void testNonChunkingReplication() {
 
-		// TODO: Check that tomcat flexrep client is alive
-
 		try {
+
+			Properties props = Utils
+					.getPropertiesFromClasspath("/flexrep-client.properties");
+
+			// Check that tomcat flexrep client is alive
+			assertTomcatFlexRepReplicaIsAlive(props);
+			
+			String xccUrl = props.getProperty("ml.flexrep.xcc.url");
+			logger.info("url=" + xccUrl);
+
 			Session session;
 			ContentCreateOptions options = new ContentCreateOptions();
-			
-			Properties props = Utils
-					.getPropertiesFromClasspath("/ml.properties");
-			String xccUrl = props.getProperty("ml.flexrep.xcc.url");
-			System.out.println("url=" + xccUrl);
-			
 			ContentSource cs = ContentSourceFactory.newContentSource(new URI(
 					xccUrl));
 			cs.setAuthenticationPreemptive(true);
 			assertMarkLogicIsAlive(cs);
-			
+
 			options.setFormatBinary();
 
-			Content content = ContentFactory.newContent("/tmp/"
-					+ NON_CHUNKING_FILENAME, ApplyServletTest.class
-					.getClassLoader()
-					.getResourceAsStream(NON_CHUNKING_FILENAME), options);
+			Content content = ContentFactory.newContent("/tmp"
+					+ NON_CHUNKING_FILENAME,
+					Utils.getClasspathContentAsStream(NON_CHUNKING_FILENAME),
+					options);
 
 			session = cs.newSession();
 			session.insertContent(content);
@@ -67,6 +77,21 @@ public class ApplyServletTest {
 		}
 	}
 
+	private void assertTomcatFlexRepReplicaIsAlive(Properties props) {
+		HttpClient client = new DefaultHttpClient();
+		HttpGet get = new HttpGet(String.format("http://%s:%s/apply.xqy",
+				props.getProperty("tomcat.http.host"),
+				props.getProperty("tomcat.http.port")));
+		try {
+			HttpResponse response = client.execute(get);
+			Assert.assertEquals(
+					"Tomcat is not alive and responding to /apply.xqy", 200,
+					response.getStatusLine().getStatusCode());
+		} catch (IOException e) {
+			Assert.fail("Tomcat is not alive and responding to /apply.xqy");
+		}
+	}
+
 	@Ignore
 	@Test
 	public void testChunkingReplication() {
@@ -79,7 +104,8 @@ public class ApplyServletTest {
 		ResultSequence sequence;
 		try {
 			sequence = session.submitRequest(query);
-			Assert.assertEquals("I'm alive", sequence.asString());
+			Assert.assertEquals("MarkLogic server is unreachable", "I'm alive",
+					sequence.asString());
 		} catch (RequestException e) {
 			Assert.fail("MarkLogic server is unreachable");
 		}
